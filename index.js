6ec756a72c14dc0e488106e19b937015fb83ce33
Drew Schuster <dschuster@slack-corp.com>
2018-08-22 13:24:29 -0700

    Comment out stuff I think I do not need

--- index.js
+++ index.js
@@ -20,44 +20,20 @@ const domain = process.env.DOMAIN;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/.well-known/host-meta', (req, res) => {
-	const xml = `<?xml version='1.0' encoding='UTF-8'?>
-<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
-  <Link rel='lrdd' type='application/json'
-        template='https://bridgy-federated.appspot.com/.well-known/webfinger?resource={uri}' />
-</XRD>`;
-	res.status(200);
-	res.type('application/xml');
-	res.send(xml);
-});
-
-// app.get('/users/:username', (req,res) => {
-// 	const username = req.params.username;
-// 	console.log(req.path, `username ${req.params.username} requested!`, req.query, req.body);
-// 	res.json({
-// 		"preferredUsername": username,
-// 		"name": "Drew Schuster (hardcoded)",
-// 		"url": `${domain}/@${username}`,
-// 		"image": [
-// 			{
-// 				"url": "https://xoxo.zone/system/accounts/avatars/000/037/322/original/009fec3fcfa521f5.jpg",
-// 				"type": "Image"
-// 			}
-// 		],
-// 		"inbox": `${domain}/users/${username}/inbox`,
-// 		"@context": "https://www.w3.org/ns/activitystreams",
-// 		"type": "Person",
-// 		"id": `${domain}/users/${username}`,
-// 		"icon": [
-// 			{
-// 				"url": "https://xoxo.zone/system/accounts/avatars/000/037/322/original/009fec3fcfa521f5.jpg",
-// 				"type": "Image"
-// 			}
-// 		]
-// 	});
-// })
+// app.get('/.well-known/host-meta', (req, res) => {
+// 	const xml = `<?xml version='1.0' encoding='UTF-8'?>
+// <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
+//   <Link rel='lrdd' type='application/json'
+//         template='https://bridgy-federated.appspot.com/.well-known/webfinger?resource={uri}' />
+// </XRD>`;
+// 	res.status(200);
+// 	res.type('application/xml');
+// 	res.send(xml);
+// });
 
 app.get('/users/:username', (req,res) => {
+	// TODO: cache these profiles.
+	// TODO: figure out how to invalidate caches for profiles as well, the other instances have their own caches
 	const username = req.params.username;
 	console.log(req.path, `username ${req.params.username} requested!`, req.query, req.body);
 	T.get('users/lookup', { screen_name: username }).then((result) => {
@@ -120,18 +96,18 @@ app.get('/.well-known/webfinger', (req, res) => {
 					"type": "application/activity+json",
 					"href": `${domain}/users/${username}`
 				},
-				{
-					"rel": "salmon",
-					"href": `${domain}/api/salmon/37322` // TODO get an id?
-				},
+				// {
+				// 	"rel": "salmon",
+				// 	"href": `${domain}/api/salmon/37322` // TODO get an id?
+				// },
 				{
 					"rel": "magic-public-key", // TODO get a real public keye
 					"href": "data:application/magic-public-key,RSA.ouYy9P4LLbEzzCNCtevEIcFbobS3USLNIRSUawGe2wuWLI8CPuRfN5Fz4ZTPfWytqwFDbF5ff4zkteizolNKJEMmMT1TD1K8SZk1lp0hvpuDK-vp8XioxVXHkyaN4JxbMRQqG62kozvB5LQvFvomMQRgQTSSjvYb7VMWbF5m6y0=.AQAB",
 				},
-				{
-					"rel": "http://ostatus.org/schema/1.0/subscribe",
-					"template": `${domain}/authorize_follow?acct={uri}`
-				}
+				// {
+				// 	"rel": "http://ostatus.org/schema/1.0/subscribe",
+				// 	"template": `${domain}/authorize_follow?acct={uri}`
+				// }
 			]
 		}
 	);

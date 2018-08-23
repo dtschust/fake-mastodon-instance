79e13496d9459a3ff9a5fc4355a52339a91b945e
Drew Schuster <dschuster@slack-corp.com>
2018-08-23 12:09:27 -0700

    trying to figure out how to post to a mastodon instance

---
+++ send-follow-request.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const request = require('request');
+const crypto = require('crypto');
+
+const message = require('./follow-request.json');
+const domain = 'https://fake-mastodon-instance.herokuapp.com'
+const privkey = process.env.INSTANCE_PRIVATE_KEY;
+const username = 'nuncamind';
+const theirDomain = 'mastodon.social';
+
+console.log(message);
+
+const signer = crypto.createSign('sha256');
+let d = new Date();
+let stringToSign = `(request-target): post /inbox\nhost: ${theirDomain}\ndate: ${d.toUTCString()}`; // TODO hardcoded host
+signer.update(stringToSign);
+signer.end;
+const signature = signer.sign(privkey);
+const signature_b64 = signature.toString('base64')
+let header = `keyId="${domain}/users/${username}",headers="(request-target) host date",signature="${signature_b64}"`;
+
+console.log('signature:', header);
+request({
+	url: `https://${theirDomain}/inbox`,
+	headers: {
+		'Host': theirDomain,
+		'Date': d.toUTCString(),
+		'Signature': header
+	},
+	method: 'POST',
+	json: true,
+	body: message
+}, function (error, response, body) {
+	console.log('YOOOOOOOOOOOOOOO', error, response.body, response.statusCode);
+});

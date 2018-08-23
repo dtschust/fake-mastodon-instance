f5c80a3e85b580091293819b0a01216433740474
Drew Schuster <dtschust@gmail.com>
2018-08-23 16:34:22 -0700

    oops

---
+++ send-message.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const request = require('request');
+const crypto = require('crypto');
+
+const domain = process.env.DOMAIN;
+
+function sendMessage(message, name, destinationDomain, cb) {
+	const signer = crypto.createSign('sha256');
+	let d = new Date();
+	let stringToSign = `(request-target): post /inbox\nhost: ${destinationDomain}\ndate: ${d.toUTCString()}`; // TODO hardcoded host
+	signer.update(stringToSign);
+	signer.end;
+	const signature = signer.sign(cert);
+	const signature_b64 = signature.toString('base64')
+	let header = `keyId="${domain}/users/${name}",headers="(request-target) host date",signature="${signature_b64}"`;
+
+	console.log('signature:', header);
+	console.log('Sending message', message);
+	request({
+		url: `https://${destinationDomain}/inbox`,
+		headers: {
+			'Host': destinationDomain,
+			'Date': d.toUTCString(),
+			'Signature': header
+		},
+		method: 'POST',
+		json: true,
+		body: message
+	}, function (error, response, body) {
+		if (cb) {
+			cb(error, response, body);
+		}
+		console.log('Response: ', error, response.body, response.statusCode);
+	});
+}

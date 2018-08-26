44e7701e8cc38b3650d6301e1adca4bec25b71ca
Drew Schuster <dtschust@gmail.com>
2018-08-25 20:16:50 -0700

    oops

--- bin/crawlTwitter.js
+++ bin/crawlTwitter.js
@@ -182,7 +182,7 @@ function postTweet(tweet) {
 	let content = `<p>${twitter.autoLinkWithJSON(
 		tweet.full_text,
 		tweet.entities,
-	)}<br> 🐦🔗 <a href="https://twitter.com/${user}/status/${id}">permalink</a></p>`;
+	)}<br> (🐦🔗 <a href="https://twitter.com/${user}/status/${id}">permalink</a>)</p>`;
 
 	// Convert @user to look like @user@twitter.com to be less confusing
 	if (entities.user_mentions && entities.user_mentions.length) {

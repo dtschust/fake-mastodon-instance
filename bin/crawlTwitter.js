ed80b21cd66e61c9a0c66554f4b69a459f5e005a
Drew Schuster <dtschust@gmail.com>
2018-08-26 14:42:25 -0700

    temp

--- bin/crawlTwitter.js
+++ bin/crawlTwitter.js
@@ -299,6 +299,7 @@ function fetchFollowers(prevFollowerIds = [], next_cursor) {
 	if (DEBUG) {
 		return Promise.resolve(mockFollowersArray);
 	}
+	return Promise.resolve(mockFollowersArray);
 
 	return T.get('friends/list', {
 		screen_name: 'nuncamind',

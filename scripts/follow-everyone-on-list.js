3c374cc9fe115b67d9c1a7ad49ec35a221cb36fe
Drew Schuster <dtschust@gmail.com>
2018-08-31 22:39:39 -0700

    Fix up follow-everyone-on-list to support if I follow more than 80 people

--- scripts/follow-everyone-on-list.js
+++ scripts/follow-everyone-on-list.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Twit = require('twit');
-const request = require('request');
+const rp = require('request-promise');
 
 const T = new Twit({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,99 +11,120 @@ const T = new Twit({
 	strictSSL: true, // optional - requires SSL certificates to be valid.
 });
 
-const domain = 'xoxo.zone';
+function grabLinkHeader(body, response) {
+	let retBody = body;
+	if (response.headers['content-type'] === 'application/json') {
+		retBody = JSON.parse(body);
+	}
+	if (response.headers.link) {
+		if (response.headers.link.indexOf('rel="next"') >= 0) {
+			const nextString = response.headers.link
+				.split(',')
+				.find(string => string.indexOf('rel="next"') >= 0);
+			const nextUrl = nextString
+				.match(/<.*>/)[0]
+				.slice(1)
+				.slice(0, -1);
+			retBody.nextUrl = nextUrl;
+		}
+	}
 
-const getAlreadyFollowing = new Promise((resolve, reject) => {
-	request(
-		{
-			url: `https://${domain}/api/v1/accounts/verify_credentials`,
-			headers: {
-				Authorization: `Bearer ${process.env.MASTODON_TOKEN}`,
-			},
-			method: 'GET',
-			json: true,
-			body: {},
+	return retBody;
+}
+
+const getLoggedInUserId = () =>
+	rp({
+		url: 'https://xoxo.zone/api/v1/accounts/verify_credentials',
+		headers: {
+			Authorization: `Bearer ${process.env.MASTODON_TOKEN}`,
 		},
-		(error, nothing, { id }) => {
-			if (error) {
-				console.log(`ERROR`, error);
-				reject();
-			} else {
-				request(
-					{
-						url: `https://${domain}/api/v1/accounts/${id}/following`,
-						headers: {
-							Authorization: `Bearer ${process.env.MASTODON_TOKEN}`,
-						},
-						method: 'GET',
-						json: true,
-						body: {
-							limit: 80,
-						},
-					},
-					(err, mastodonResponse, followingBody) => {
-						if (error) {
-							console.log(`ERROR`);
-							reject();
-						} else {
-							if (followingBody.length === 80) {
-								// TODO support more than 80 following by refetching
-								throw new Error('Cannot trust this list, it is incomplete!');
-							}
+		method: 'GET',
+		json: true,
+		body: {},
+	}).then(({ id }) => id);
 
-							const alreadyFollowing = followingBody
-								.filter(({ acct }) => acct.split('@')[1] === 'toot.rip')
-								.map(({ username }) => username.toLowerCase());
-							resolve(alreadyFollowing);
-						}
-					},
+const getFollowingById = (
+	id,
+	domain,
+	url = `https://${domain}/api/v1/accounts/${id}/following`,
+	followingUsernames = [],
+) => {
+	console.log('fetching', url);
+	return rp({
+		url,
+		headers: {
+			Authorization: `Bearer ${process.env.MASTODON_TOKEN}`,
+		},
+		method: 'GET',
+		json: true,
+		body: {
+			limit: 80,
+		},
+		transform: grabLinkHeader,
+	})
+		.then(followingBody => {
+			// eslint-disable-next-line no-param-reassign
+			followingUsernames = followingUsernames.concat(
+				followingBody
+					.filter(({ acct }) => acct.split('@')[1] === 'toot.rip')
+					.map(({ username }) => username.toLowerCase()),
+			);
+			if (followingBody.nextUrl) {
+				return getFollowingById(
+					id,
+					domain,
+					followingBody.nextUrl,
+					followingUsernames,
 				);
 			}
-		},
-	);
-});
+
+			return followingUsernames;
+		})
+		.catch(err => {
+			console.log(`ERROR`, err);
+		});
+};
+
+const getMyFollowing = () =>
+	getLoggedInUserId().then(id => getFollowingById(id, 'xoxo.zone'));
 
 const getTwitterListMembers = T.get('lists/members', {
 	list_id: '14507192',
 	count: 5000,
 });
 
-Promise.all([getAlreadyFollowing, getTwitterListMembers]).then(
+Promise.all([getMyFollowing(), getTwitterListMembers]).then(
 	([alreadyFollowing, response]) => {
-		// eslint-disable-next-line camelcase
-		const usernames = response.data.users.map(({ screen_name }) => screen_name);
-		const promises = usernames.map(username => {
-			if (alreadyFollowing.indexOf(username.toLowerCase()) !== -1) {
-				// console.log(`skipping ${username} because we already follow them`);
-				return Promise.resolve();
-			}
-			return new Promise((resolve, reject) => {
-				request(
-					{
-						url: `https://${domain}/api/v1/follows`,
-						headers: {
-							Authorization: `Bearer ${process.env.MASTODON_TOKEN}`,
-						},
-						method: 'POST',
-						json: true,
-						body: {
-							uri: `${username}@toot.rip`,
-						},
+		const domain = 'xoxo.zone';
+		const usernames = response.data.users
+			// eslint-disable-next-line camelcase
+			.map(({ screen_name }) => screen_name)
+			.filter(
+				username => alreadyFollowing.indexOf(username.toLowerCase()) === -1,
+			);
+		console.log('to follow: ', usernames, usernames.length);
+		Promise.all(
+			usernames.map(username =>
+				rp({
+					url: `https://${domain}/api/v1/follows`,
+					headers: {
+						Authorization: `Bearer ${process.env.MASTODON_TOKEN}`,
 					},
-					(error, mastodonResponse, body) => {
-						if (error) {
-							console.log(`ERROR: COULD NOT FOLLOW ${username}!`);
-							reject(error);
-						} else {
-							console.log(`Successfully followed ${username}!`);
-							resolve(body);
-						}
+					method: 'POST',
+					json: true,
+					body: {
+						uri: `${username}@toot.rip`,
 					},
-				);
-			});
-		});
-
-		Promise.all(promises).then(() => {
+				})
+					.then(() => {
+						console.log(`Successfully followed ${username}!`);
+					})
+					.catch(error => {
+						console.log(`ERROR: COULD NOT FOLLOW ${username}!`);
+						throw error;
+					}),
+			),
+		).then(() => {
 			console.log('done!');
 		});
 	},
